83df07d75ebea4b32179763e0b39e37b41c16b81
LindenAdamson <98446994+LindenAdamson@users.noreply.github.com>
2024-10-12 07:02:32 -0400

    almost finished

--- test_lane_detection.py
+++ test_lane_detection.py
@@ -1,29 +1,50 @@
 from lane_detection import LaneDetection
+from rgb_helper import Camera
 import matplotlib.pyplot as plt
 import numpy as np
 import pyglet
 from pyglet import gl
 from pyglet.window import key
+import threading
+import time
+import cv2
+
+steps = 0
 
 # init carla environement
-
-# define variables
-steps = 0
+camera = Camera()
 
 # init modules of the pipeline
 LD_module = LaneDetection()
 
 # init extra plot
-fig = plt.figure()
+fig = plt.figure("gt")
+fig_test = plt.figure("test")
 plt.ion()
 plt.show()
 
+def start_camera():
+    camera.run()
+
+t1 = threading.Thread(target=start_camera, args=())
+
+t1.start()
+time.sleep(5)
+
+index = 0
 while True:
     # perform step
-
     # lane detection
-    splines = LD_module.lane_detection(s)
-    
+
+    if(index == camera.get_index()):
+        continue
+    index = camera.get_index()
+    image = cv2.imread("frame.jpg")
+    s = np.asarray(image)
+    if(not s.size):
+        continue
+
+    splines = LD_module.lane_detection(s, fig_test)
     # plot figure
     if steps % 2 == 0:
         LD_module.plot_state_lane(s, steps, fig)
