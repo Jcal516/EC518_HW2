1a4144c45409cc499f6ed0772814f30803ace996
JasonCalalang <jasonc16@bu.edu>
2024-10-18 16:08:54 -0400

    waypoint update

--- test_waypoint_prediction.py
+++ test_waypoint_prediction.py
@@ -1,34 +1,56 @@
 from lane_detection import LaneDetection
+from rgb_helper import Camera
 from waypoint_prediction import waypoint_prediction, target_speed_prediction
 import matplotlib.pyplot as plt
 import numpy as np
 import pyglet
 from pyglet import gl
 from pyglet.window import key
+import threading
+import time
+import cv2
+
+steps = 0
 
 # init carla environement
-
-# define variables
-steps = 0
+camera = Camera()
 
 # init modules of the pipeline
 LD_module = LaneDetection()
 
 # init extra plot
-fig = plt.figure()
+fig = plt.figure("gt")
+fig_test = plt.figure("test")
 plt.ion()
 plt.show()
 
+def start_camera():
+    camera.run()
+
+t1 = threading.Thread(target=start_camera, args=())
+
+t1.start()
+time.sleep(5)
+
+index=0;
 while True:
     # perform step
     
     # lane detection
-    lane1, lane2 = LD_module.lane_detection(s)
+    if(index == camera.get_index()):
+        continue
+    index = camera.get_index()
+    image = cv2.imread("frame.jpg")
+    s = np.asarray(image)
+    if(not s.size):
+        continue
+    
+    lane1, lane2 = LD_module.lane_detection(s, fig_test)
 
     # waypoint and target_speed prediction
     waypoints = waypoint_prediction(lane1, lane2)
     target_speed = target_speed_prediction(waypoints)
-
+    print(target_speed)
     # plot figure
     if steps % 2 == 0:
         LD_module.plot_state_lane(s, steps, fig, waypoints=waypoints)
--- waypoint_prediction.py
+++ waypoint_prediction.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
-from scipy.interpolate import splprep, splev
+from scipy.interpolate import splprep, splev, BSpline
 from scipy.optimize import minimize
 import time
 
 
 def normalize(v):
+    #print(v.shape)
     norm = np.linalg.norm(v,axis=0) + 0.00001
     return v / norm.reshape(1, v.shape[1])
 
@@ -19,7 +20,22 @@ def curvature(waypoints):
     args: 
         waypoints [2, num_waypoints] !!!!!
     '''
+    b = 1
+    curvature = 0
+    for a in range(waypoints.shape[1]-2):
+        vec1 = waypoints[:,a+1]-waypoints[:,a]
 
+        vec2 = waypoints[:,a+2]-waypoints[:,a+1]
+        #print(vec1)
+        #print(vec2)
+        v = np.append(vec1,vec2)
+        v = normalize(v.reshape(2,2))
+        dotproduct = np.dot(v[:,0],v[:,1])
+
+        curvature += dotproduct
+            
+    curvature = curvature * b
+    
     return curvature
 
 
@@ -33,6 +49,9 @@ def smoothing_objective(waypoints, waypoints_center, weight_curvature=40):
         weight_curvature (default=40)
     '''
     # mean least square error between waypoint and way point center
+    #print(waypoints_center.shape)
+    #print(waypoints)
+    waypoints = waypoints.reshape(2,-1)
     ls_tocenter = np.mean((waypoints_center - waypoints)**2)
 
     # derive curvature
@@ -59,11 +78,25 @@ def waypoint_prediction(roadside1_spline, roadside2_spline, num_waypoints=6, way
         ##### TODO #####
      
         # create spline arguments
+        print(type(roadside1_spline))
+        t1,c1,k1 = roadside1_spline
+        t2,c2,k2 = roadside2_spline
+    
 
+        
+        new1_spline = BSpline(t1, np.array(c1).T, k1)
+        new2_spline = BSpline(t2, np.array(c2).T, k2)
         # derive roadside points from spline
-
+        x = np.linspace(0,1,num_waypoints)
+        roadside1_points = np.array(splev(x,roadside1_spline)).T
+        roadside2_points = np.array(splev(x,roadside2_spline)).T
+        print(roadside1_points)
+        print(roadside2_points)
         # derive center between corresponding roadside points
-
+        way_points = (roadside1_points + roadside2_points) / 2
+        print("way")
+        print(way_points)
+        print(way_points.shape)
         # output way_points with shape(2 x Num_waypoints)
         return way_points
     
@@ -71,22 +104,36 @@ def waypoint_prediction(roadside1_spline, roadside2_spline, num_waypoints=6, way
         ##### TODO #####
 
         # create spline arguments
+        #print("smooth")
+        #print(roadside1_spline)
+        print(type(roadside1_spline))
+        if isinstance(roadside1_spline, int):
+            return roadside1_spline
+        (t1,c1,k1) = roadside1_spline
+        (t2,c2,k2) = roadside2_spline
 
+        new1_spline = BSpline(t1, np.array(c1).T, k1)
+        new2_spline = BSpline(t2, np.array(c2).T, k2)
         # derive roadside points from spline
-
+        x = np.linspace(0,1,2*num_waypoints)
+        roadside1_points = np.array(splev(x,roadside1_spline)).T
+        roadside2_points = np.array(splev(x,roadside2_spline)).T
+        #print(roadside1_points)
+        #print(roadside2_points)
         # derive center between corresponding roadside points
-        way_point_center = 
-        
+        print(roadside1_points.shape)
+        way_points_center = (roadside1_points.reshape(2,-1) + roadside2_points.reshape(2,-1)) / 2
+        #print(way_points_center)
         # optimization
         way_points = minimize(smoothing_objective, 
                       (way_points_center), 
                       args=way_points_center)["x"]
 
-        return way_points.reshape(2,-1)
+        return way_points.reshape(-1,2)
 
 
 def target_speed_prediction(waypoints, num_waypoints_used=5,
-                            max_speed=60, exp_constant=4.5, offset_speed=30):
+                            max_speed=60, exp_constant=.4, offset_speed=30):
     '''
     ##### TODO #####
     Predict target speed given waypoints
@@ -102,6 +149,16 @@ def target_speed_prediction(waypoints, num_waypoints_used=5,
     output:
         target_speed (float)
     '''
-
+    waypoints = waypoints.reshape(2,-1)
+    curv = curvature(waypoints[:,:2*num_waypoints_used:2])
+    print("curv=")
+    print(curv)
+    curv_new = num_waypoints_used-2-curv
+    print("num_waypoints - 2 - curve = ")
+    print(curv_new)
+    print("exp = ")
+    exponent = -1 * exp_constant * np.abs(curv)
+    print(exponent)
+    target_speed = ((max_speed - offset_speed) * np.exp(exponent)) + offset_speed
 
     return target_speed
