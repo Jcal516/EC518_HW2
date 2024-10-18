07a9561d02ce3017bfebadbf73ac63432aa412f2
LindenAdamson <98446994+LindenAdamson@users.noreply.github.com>
2024-10-18 17:39:47 -0400

    final version

--- test_lateral_control.py
+++ test_lateral_control.py
@@ -6,6 +6,10 @@ import numpy as np
 import pyglet
 from pyglet import gl
 from pyglet.window import key
+from rgb_helper import Camera
+import threading
+import time
+import cv2
 
 # action variables
 
@@ -14,31 +18,57 @@ from pyglet.window import key
 # define variables
 steps = 0
 
+# init carla environement
+camera = Camera()
+
 # init modules of the pipeline
 LD_module = LaneDetection()
 LatC_module = LateralController()
 
 # init extra plot
-fig = plt.figure()
+fig = plt.figure("gt")
+fig_test = plt.figure("test")
 plt.ion()
 plt.show()
 
+def start_camera(): # take input from a carla autopilot
+    camera.run()
+
+t1 = threading.Thread(target=start_camera, args=())
+
+t1.start()
+time.sleep(5)
+
+index = 0
 while True:
     # perform step
 
+    if(index == camera.get_index()):
+        continue
+    index = camera.get_index()
+    image = cv2.imread("frame.jpg")
+    s = np.asarray(image)
+    if(s.size != 320*240*3):
+        continue
+    f = open("speed.txt", "r")
+    speed = float(f.read())
+
     # lane detection
-    lane1, lane2 = LD_module.lane_detection(s)
+    lane1, lane2 = LD_module.lane_detection(s, fig_test)
 
     # waypoint and target_speed prediction
     waypoints = waypoint_prediction(lane1, lane2)
     target_speed = target_speed_prediction(waypoints)
 
     # control with constant gas and no braking
-    a[0] = LatC_module.stanley(waypoints, speed)
+    a = LatC_module.stanley(waypoints, speed)
+
+    #print("waypoints:")
+    #print(waypoints)
 
     # output and plot figure
     if steps % 2 == 0:
-        print("\naction " + str(["{:+0.2f}".format(x) for x in a]))
+        print("\naction " + str("{:+0.2f}".format(a)))
         print("targetspeed {:+0.2f}".format(target_speed))
         LD_module.plot_state_lane(s, steps, fig, waypoints=waypoints)
     steps += 1
