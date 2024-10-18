07e29c4326d655f9c0caec2b15e38ab61dde3302
LindenAdamson <98446994+LindenAdamson@users.noreply.github.com>
2024-10-18 17:40:05 -0400

    final version

--- test_longitudinal_control.py
+++ test_longitudinal_control.py
@@ -7,10 +7,15 @@ import numpy as np
 import pyglet
 from pyglet import gl
 from pyglet.window import key
+from rgb_helper import Camera
+import threading
+import time
+import cv2
 
 # action variables
 
 # init carla environement
+camera = Camera()
 
 # define variables
 steps = 0
@@ -21,15 +26,37 @@ LatC_module = LateralController()
 LongC_module = LongitudinalController()
 
 # init extra plot
-fig = plt.figure()
+fig = plt.figure("gt")
+fig_test = plt.figure("test")
 plt.ion()
 plt.show()
 
+a = [0,0,0]
+
+def start_camera(): # take input from a carla autopilot
+    camera.run()
+
+t1 = threading.Thread(target=start_camera, args=())
+
+t1.start()
+time.sleep(5)
+
+index = 0
 while True:
     # perform step
 
+    if(index == camera.get_index()):
+        continue
+    index = camera.get_index()
+    image = cv2.imread("frame.jpg")
+    s = np.asarray(image)
+    if(s.size != 320*240*3):
+        continue
+    f = open("speed.txt", "r")
+    speed = float(f.read())
+
     # lane detection
-    lane1, lane2 = LD_module.lane_detection(s)
+    lane1, lane2 = LD_module.lane_detection(s)#, fig_test)
 
     # waypoint and target_speed prediction
     waypoints = waypoint_prediction(lane1, lane2)
